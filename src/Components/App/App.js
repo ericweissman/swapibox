4dde4c4e1a43d33e549c91db35f2cab3e88efdb4
Eric Weissman <eric.weissman21@gmail.com>
2019-01-23 16:20:22 -0700

    Adds fetch calls and functions for people, their homeworlds and their species

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,9 +9,51 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      people: [],
     }
   }
+
+fetchPeopleData() {
+  fetch('https://swapi.co/api/people/?page=1')
+    .then(response => response.json())
+    .then(people => this.fetchHomeworlds(people.results))
+    .then(peopleWithHomes => this.fetchSpecies(peopleWithHomes))
+    .then(result => console.log(result))
+  }
+
+fetchSpecies(people) {
+  const unresolvedPromises = people.map((person) => {
+    return fetch(person.species)
+      .then(response => response.json())
+      .then(speciesData => ({
+        ...person,
+        species: speciesData.name
+      }))
+  })
+
+  return Promise.all(unresolvedPromises)
+}
+
+fetchHomeworlds(people) {
+  const unresolvedPromises = people.map((person) => {
+    return fetch(person.homeworld)
+      .then(response => response.json())
+      .then(homeworldData => ({ 
+        ...person, 
+        homeworld: homeworldData.name,
+        population: homeworldData.population,
+      }))
+  })
+
+  return Promise.all(unresolvedPromises)
+}
+
+
+
+  async componentDidMount() {
+    this.fetchPeopleData();
+  }
+
   render() {
     return (
       <div className="App">
