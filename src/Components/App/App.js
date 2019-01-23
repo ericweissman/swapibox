34fd2fa3e637abde95fef1a2fa70908149ce987d
Eric Weissman <eric.weissman21@gmail.com>
2019-01-23 07:40:37 -0700

    Connects MovieText to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import '../../Main.scss';
+import MovieText from '../MovieText/MovieText'
+
 
 class App extends Component {
   constructor() {
     super();
-    
+    this.state = {
+
+    }
   }
   render() {
     return (
       <div className="App">
-        
+        <MovieText />
       </div>
     );
   }
---
+++ src/Components/MovieText/MovieText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieText = (props) => {
+  return(
+    <div>
+      Movie Text Here
+    </div>
+  )
+}
+
+export default MovieText;
