eb107942ce6b854890af8870df9f8ebc7cb54e6e
Eric Weissman <eric.weissman21@gmail.com>
2019-01-23 07:50:04 -0700

    Connects Card Container to app

---
+++ src/Components/CardContainer/CardContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CardContainer = (props) => {
+  return(
+    <div>CardContainer</div>
+  )
+}
+
+export default CardContainer;
--- src/Components/Controls/Controls.js
+++ src/Components/Controls/Controls.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Controls extends Component {
   constructor() {
