90c615127844bebcf7179b39869172f4d06a9b20
Eric Weissman <eric.weissman21@gmail.com>
2019-01-24 21:09:28 -0700

    Adds adjustments for movie crawl display

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -129,7 +129,7 @@ fetchCrawl = async () => {
 componentDidMount = () =>  {
   this.fetchPeopleData();
   this.fetchCrawl();
-  // this.fetchPlanetData();
+  this.fetchPlanetData();
 }
 
   render() {
