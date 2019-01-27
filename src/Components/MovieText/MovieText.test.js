ef6fac65ef33bd0dfb8b37962e49f74a439730fe
Eric Weissman <eric.weissman21@gmail.com>
2019-01-27 11:26:17 -0700

    Adds snapshot for movieText

---
+++ src/Components/MovieText/MovieText.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MovieText from './MovieText'
+import { shallow } from 'enzyme'
+
+describe('MovieText', () => {
+  const films = {title: 'Title', crawl: 'Text', year: '1996'}
+
+  it('should match snapshot', () => {
+   const wrapper = shallow(<MovieText films={films} />)
+   expect(wrapper).toMatchSnapshot();
+  });
+
+  
+
+})
